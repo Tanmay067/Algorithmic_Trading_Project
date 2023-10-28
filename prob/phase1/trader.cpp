da283b20b62eb4990997b962194792a0df8d8c9b
aryaman-angurana <aryamanangurana26@gmail.com>
2023-10-28 18:26:16 +0530

    added tokenizer

--- prob/phase1/trader.cpp
+++ prob/phase1/trader.cpp
@@ -1,27 +1,55 @@
 #include "receiver.h"
+#include <vector>
 
-int main() {
-
+int main(int argc, char **argv)
+{
     Receiver rcv;
-    sleep(5);
+    sleep(3);
     std::string message = rcv.readIML();
-    int spaces = 0;
+    std::vector<std::vector<std::string>> inputs;
+    std::vector<std::string> lines = {""};
+    inputs.push_back(lines);
+    int j = 0;
+    int k = 0;
     for (char i : message)
     {
-        if (i == ' ') spaces++;
-        if (i == '#') break;
+        if (i == '$')
+            break;
+        else if (i == '#')
+        {
+            j = 0;
+            k++;
+        }
+        else if ((int)i == 13)
+        {
+            inputs.push_back(lines);
+        }
+        else if (i == ' ')
+        {
+            j++;
+            inputs[k].push_back("");
+        }
+        else
+        {
+            inputs[k][j].push_back(i);
+        }
     }
-    if (spaces == 2)
+    if (argc == 1)
     {
-        
     }
-    else if (spaces % 2 == 1)
+    else if (argc == 2)
     {
-
+        for (std::vector<std::string> i : inputs)
+        {
+            for (std::string x : i)
+            {
+                std::cout << x;
+                std::cout << std::endl;
+            }
+        }
     }
-    else if (spaces % 2 == 0)
+    else if (argc == 3)
     {
-
     }
     else
     {
