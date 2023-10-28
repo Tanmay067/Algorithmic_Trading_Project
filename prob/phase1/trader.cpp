b41fb67cfacc2853caae0a0e5782a4e33dfdc244
aryaman-angurana <aryamanangurana26@gmail.com>
2023-10-28 16:12:59 +0530

    added space framework for phase1

--- prob/phase1/trader.cpp
+++ prob/phase1/trader.cpp
@@ -5,6 +5,28 @@ int main() {
     Receiver rcv;
     sleep(5);
     std::string message = rcv.readIML();
+    int spaces = 0;
+    for (char i : message)
+    {
+        if (i == ' ') spaces++;
+        if (i == '#') break;
+    }
+    if (spaces == 2)
+    {
+        
+    }
+    else if (spaces % 2 == 1)
+    {
+
+    }
+    else if (spaces % 2 == 0)
+    {
+
+    }
+    else
+    {
+        return 0;
+    }
     std::cout << message;
     return 0;
 }
